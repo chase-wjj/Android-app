ff4966b7c96840b97a92bbb83c46b5522ee17014
wjy <wjytsinghua@126.com>
2023-06-08 23:44:25 +0800

    add chat

--- wwwork/app/src/main/java/org/tsinghua/todoapp/FriendFragment.java
+++ wwwork/app/src/main/java/org/tsinghua/todoapp/FriendFragment.java
@@ -42,18 +42,7 @@ public class FriendFragment extends Fragment {
         });
 
         return view;
-<<<<<<< HEAD
-    }
 
-    private void initFriendList() {
-        Friend friend1 = new Friend("张三", "13812345678", R.drawable.person);
-        friendList.add(friend1);
-        Friend friend2 = new Friend("李四", "13912345678", R.drawable.person);
-        friendList.add(friend2);
-        Friend friend3 = new Friend("王五", "13712345678", R.drawable.person);
-        friendList.add(friend3);
-=======
->>>>>>> 902c860611a63671c1522e3c29c74da004113b5f
     }
 
     private void initFriendList() {
@@ -64,4 +53,5 @@ public class FriendFragment extends Fragment {
         Friend friend3 = new Friend("王五", "13712345678", R.drawable.person);
         friendList.add(friend3);
     }
+
 }
